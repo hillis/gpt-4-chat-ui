33ef3f0d16e765e2ae0d3a519009617515b3ed83
Matthew Hillis <843288+hillis@users.noreply.github.com>
2023-03-27 11:23:03 -0500

    Initial

---
+++ pages/api/chat.ts
@@ -0,0 +1,34 @@
+// Make sure to add OPENAI_API_KEY as a secret
+
+import {
+  Configuration,
+  OpenAIApi,
+  ChatCompletionRequestMessageRoleEnum,} from "openai";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function chatHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const completion = await openai.createChatCompletion({
+    // Downgraded to GPT-3.5 due to high traffic. Sorry for the inconvenience.
+    // If you have access to GPT-4, simply change the model to "gpt-4"
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        "role": ChatCompletionRequestMessageRoleEnum.System,
+        "content": "You are a helpful assistant.",
+      },
+    ].concat(req.body.messages),
+  });
+  res.status(200).json({ result: completion.data.choices[0].message });
+}
+
+export default chatHandler;
