ecb4e47e58f96901ae8342a4e63aed1c0e3c3b9d
Matthew Hillis <843288+hillis@users.noreply.github.com>
2023-03-27 14:42:44 -0500

    Integrating Searching Documents

---
+++ pages/lib/qaHandler.ts
@@ -0,0 +1,31 @@
+// lib/qaHandler.ts
+
+import { OpenAI } from "langchain/llms";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores";
+import { OpenAIEmbeddings } from "langchain/embeddings";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import * as fs from "fs";
+
+export const run = async (question: string, chatHistory: string = "") => {
+  /* Initialize the LLM to use to answer the question */
+  const model = new OpenAI({});
+  /* Load in the file we want to do question answering over */
+  const text = fs.readFileSync("state_of_the_union.txt", "utf8");
+  /* Split the text into chunks */
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+  const docs = await textSplitter.createDocuments([text]);
+  /* Create the vectorstore */
+  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+  /* Create the chain */
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorStore.asRetriever()
+  );
+  /* Ask it a question */
+  const res = await chain.call({
+    question,
+    chat_history: chatHistory.split("\n"),
+  });
+  return res;
+};
