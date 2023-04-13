59eb318e42b1c37d36a21199bea098a8b3900c42
Matthew Hillis <843288+hillis@users.noreply.github.com>
2023-04-13 07:33:49 -0500

    AI is Sarcastic

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,36 +1,44 @@
-// Make sure to add OPENAI_API_KEY as a secret
-
-import {
-  Configuration,
-  OpenAIApi,
-  ChatCompletionRequestMessageRoleEnum,} from "openai";
+import axios, { AxiosResponse } from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+async function chatHandler(req: NextApiRequest, res: NextApiResponse) {
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+  };
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-async function chatHandler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const completion = await openai.createChatCompletion({
-    // Downgraded to GPT-3.5 due to high traffic. Sorry for the inconvenience.
-    // If you have access to GPT-4, simply change the model to "gpt-4"
+  const data = {
     model: "gpt-4",
+    stream: true,
     messages: [
       {
-        role: ChatCompletionRequestMessageRoleEnum.System,
-        content: "You are a helpful assistant.",
+        role: "system",
+        content: "You are a funny sarcastic assistant.",
       },
-      
     ].concat(req.body.messages),
-    temperature: 0,
-  });
-  res.status(200).json({ result: completion.data.choices[0].message });
+    temperature: 0.9,
+  };
+
+  try {
+    const response: AxiosResponse<any> = await axios.post(
+      "https://api.openai.com/v1/engines/davinci-codex/completions",
+      data,
+      {
+        headers: headers,
+        responseType: "stream",
+      }
+    );
+
+    response.data.on("data", (chunk: Buffer) => {
+      res.write(chunk);
+    });
+
+    response.data.on("end", () => {
+      res.status(200).end();
+    });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 export default chatHandler;
